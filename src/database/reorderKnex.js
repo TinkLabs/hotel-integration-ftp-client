37928878ec57bbc6531e4aeacbd35a7887c7c99b
linhan.cai <linhan.cai@hi.com>
2019-04-25 23:06:43 +0800

    order file

--- src/database/reorderKnex.js
+++ src/database/reorderKnex.js
@@ -15,26 +15,28 @@ const dbConfig = {
 };
 const reorderKnex = require('knex')(dbConfig);
 
-const ENUM_REORDER_STATUS = {
-  CLIENT_SEND: 'client_send',
-  REORDER_RECEIVING: 'reorder_receiving',
-  REORDER_RECEIVED: 'reorder_received',
-  REORDER_SENDING: 'reorder_sending',
-  REORDER_FINISH: 'reorder_finish',
-  REORDER_ERROR: 'reorder_error',
-  CLIENT_FINISH: 'client_finish',
+const REORDER_STATUS = {
+  INIT: 'reorder_init',
+  RECEIVING: 'reorder_receiving',
+  RECEIVED: 'reorder_received',
+  PENDING: 'reorder_pending',
+  SENDING: 'reorder_sending',
+  FINISH: 'reorder_finish',
+  ERROR: 'reorder_error',
+  TIMEOUT: 'reorder_timeout',
 };
 
 /**
  * @param uniqueFileId
  * @param chunkRecordsLength
  */
-function initReorderMessage(uniqueFileId, chunkRecordsLength) {
+function initReorderMessage(hotelId, uniqueFileId, chunkRecordsLength) {
   return reorderKnex('reservation_reorder_messages').insert({
     id: uniqueFileId,
+    hotel_id: hotelId,
     chunk_count: chunkRecordsLength,
     chunk_received_count: 0,
-    status: ENUM_REORDER_STATUS.CLIENT_SEND,
+    status: REORDER_STATUS.INIT,
     modify_at: reorderKnex.fn.now(),
     create_at: reorderKnex.fn.now(),
   });
@@ -42,6 +44,6 @@ function initReorderMessage(uniqueFileId, chunkRecordsLength) {
 
 export {
   reorderKnex,
-  ENUM_REORDER_STATUS,
+  REORDER_STATUS,
   initReorderMessage,
 };
--- src/systems/order-handler.js
+++ src/systems/order-handler.js
@@ -2,7 +2,7 @@
 import uuid from 'uuid/v4';
 import Promise from 'bluebird';
 import AWS from 'aws-sdk';
-import { reorderKnex, ENUM_REORDER_STATUS } from '../database/reorderKnex';
+import { reorderKnex, REORDER_STATUS } from '../database/reorderKnex';
 
 let sqs = new AWS.SQS({ region: 'ap-southeast-1' });
 
@@ -39,21 +39,23 @@ async function receiveChunk(raw) {
   let chunkCount = meta.reorder_chunk_count;
   let chunkId = meta.chunk_id;
   let chunkSeq = meta.reorder_chunk_seq;
+  let hotelId = meta.hotel_id;
 
   // If message record not exists, try to create one.
   let row = await reorderKnex('reservation_reorder_messages').where('id', uniqueId).first('*');
   if (row === undefined) {
     reorderKnex('reservation_reorder_messages').insert({
       id: uniqueId,
+      hotel_id: hotelId,
       chunk_count: chunkCount,
       chunk_received_count: 0,
-      status: ENUM_REORDER_STATUS.REORDER_RECEIVING,
+      status: REORDER_STATUS.RECEIVING,
       modify_at: reorderKnex.fn.now(),
       create_at: reorderKnex.fn.now(),
     });
-  } else if (row.status === ENUM_REORDER_STATUS.CLIENT_SEND) {
-    reorderKnex('reservation_reorder_messages').where('id', uniqueId).update({ status: ENUM_REORDER_STATUS.REORDER_RECEIVING, modify_at: reorderKnex.fn.now() }).then(() => {});
-  } else if (row.status !== ENUM_REORDER_STATUS.REORDER_RECEIVING) {
+  } else if (row.status === REORDER_STATUS.INIT) {
+    reorderKnex('reservation_reorder_messages').where('id', uniqueId).update({ status: REORDER_STATUS.RECEIVING, modify_at: reorderKnex.fn.now() }).then(() => { });
+  } else if (row.status !== REORDER_STATUS.RECEIVING) {
     throw new Error(`message ${row.id} status ${row.status} but recerve a new message`);
   }
 
@@ -66,37 +68,102 @@ async function receiveChunk(raw) {
     create_at: reorderKnex.fn.now(),
   });
 
-  let sql = 'UPDATE reservation_reorder_messages SET modify_at = ?, chunk_received_count = chunk_received_count + 1, status = CASE chunk_received_count WHEN chunk_count THEN ? ELSE status END WHERE id = ?';
-  await reorderKnex.raw(sql, [reorderKnex.fn.now(), ENUM_REORDER_STATUS.REORDER_RECEIVED, uniqueId]);
+  let sql = 'UPDATE reservation_reorder_messages SET modify_at = ?, chunk_received_count = chunk_received_count + 1 WHERE id = ?';
+  await reorderKnex.raw(sql, [reorderKnex.fn.now(), uniqueId]);
 }
 
 async function reorderChunk() {
   // send to hai queue one by one;
-  let message = await reorderKnex('reservation_reorder_messages').where('status', ENUM_REORDER_STATUS.REORDER_RECEIVED).first('*');
+  let message = await reorderKnex('reservation_reorder_messages').where('status', REORDER_STATUS.PENDING).first('*');
   if (message === undefined) {
     return;
   }
 
   let messageId = message.id;
-  await reorderKnex('reservation_reorder_messages').where('id', messageId).update({ status: ENUM_REORDER_STATUS.REORDER_SENDING, modify_at: reorderKnex.fn.now() });
+  await reorderKnex('reservation_reorder_messages').where('id', messageId).update({ status: REORDER_STATUS.SENDING, modify_at: reorderKnex.fn.now() });
   try {
     for (let i = 0; i < message.chunk_count; i += 1) {
       // eslint-disable-next-line no-await-in-loop
       let chunk = await reorderKnex('reservation_reorder_message_chunks').where({ message_id: messageId, sequence: i }).first('*');
       // eslint-disable-next-line no-await-in-loop
-      await sendToHaiQueue(chunk.content);
+      if (chunk && chunk.content) {
+        await sendToHaiQueue(chunk.content);
+      }
+      // just update the record! less is better than nothing.
+      reorderKnex('reservation_reorder_messages').where('id', messageId).update({ modify_at: reorderKnex.fn.now() }).then(() => { });
     }
 
-    await reorderKnex('reservation_reorder_messages').where('id', messageId).update({ status: ENUM_REORDER_STATUS.REORDER_FINISH, modify_at: reorderKnex.fn.now() });
+    await reorderKnex('reservation_reorder_messages').where('id', messageId).update({ status: REORDER_STATUS.FINISH, modify_at: reorderKnex.fn.now() });
     await reorderKnex('reservation_reorder_message_chunks').where({ message_id: messageId }).delete();
     console.log(`[reorderChunk] success reorder message ${messageId} to SQS, chunk count ${message.chunk_count}`);
   } catch (err) {
     console.log(`[reorderChunk] error reorder message ${messageId} to SQS, chunk count ${message.chunk_count}, err ${err}`);
-    await reorderKnex('reservation_reorder_messages').where('id', messageId).update({ status: ENUM_REORDER_STATUS.REORDER_ERROR, modify_at: reorderKnex.fn.now() });
+    await reorderKnex('reservation_reorder_messages').where('id', messageId).update({ status: REORDER_STATUS.ERROR, modify_at: reorderKnex.fn.now() });
   }
+
+  setTimeout(reorderChunk, 1000);
+}
+
+async function monitorMessage() {
+  // if chunk_count eq chunk_received_count, update status to received
+  await reorderKnex('reservation_reorder_messages')
+    .where('status', REORDER_STATUS.RECEIVING)
+    .whereRaw('`chunk_received_count` = `chunk_count`')
+    .update({ status: REORDER_STATUS.RECEIVED, modify_at: reorderKnex.fn.now() });
+
+  // upload status from RECEIVED to PENDING, only for the first record of each hotel
+  reorderKnex('reservation_reorder_messages')
+    .where('status', REORDER_STATUS.RECEIVED)
+    .distinct('hotel_id')
+    .then((hotels) => {
+      hotels.forEach((hotel) => {
+        reorderKnex('reservation_reorder_messages')
+          .where('hotel_id', hotel.hotel_id)
+          .whereIn('status', [
+            REORDER_STATUS.INIT,
+            REORDER_STATUS.RECEIVING,
+            REORDER_STATUS.RECEIVED,
+            REORDER_STATUS.SENDING,
+            REORDER_STATUS.PENDING,
+          ])
+          .orderBy('create_at', 'asc')
+          .first()
+          .then((record) => {
+            if (record.status === REORDER_STATUS.RECEIVED) {
+              reorderKnex('reservation_reorder_messages').where('id', record.id).update({ status: REORDER_STATUS.PENDING, modify_at: reorderKnex.fn.now() }).then(() => { });
+            } else if (new Date() - record.modify_at > 60 * 1000) {
+              // 一般都是发送到HAI-QUEUE的过程中，关闭了进程导致的，可以考虑自动回滚为PENDING，暂时手工处理吧
+              // if (record.status === REORDER_STATUS.PENDING) {
+              // eslint-disable-next-line max-len
+              //   reorderKnex('reservation_reorder_messages').where('id', record.id).update({ status: REORDER_STATUS.PENDING, modify_at: reorderKnex.fn.now() }).then(() => { });
+              // }
+              console.log(`record ${JSON.stringify(record)} block queue more than 1 Hour ${new Date() - record.modify_at}`);
+            }
+          });
+      });
+    });
+
+  // timeout.
+  // if record keep one status more than ten minutes. set it to ERROR.
+  // reorderKnex('reservation_reorder_messages')
+  //   .whereIn('status', [
+  //     REORDER_STATUS.INIT,
+  //     REORDER_STATUS.RECEIVING,
+  //     REORDER_STATUS.SENDING,
+  //   ])
+  //   .whereRaw('NOW() > DATE_ADD(`modify_at`, INTERVAL 1 Hour)')
+  //   .select()
+  //   .then((records) => { if (records.length > 0) { console.log(typeof records, `record ${JSON.stringify(records)} block queue more than 1 Hour`); } });
+  // .update({ status: REORDER_STATUS.TIMEOUT, modify_at: reorderKnex.fn.now() })
+  // .then(() => { });
+
+  setTimeout(monitorMessage, 2000);
 }
 
 export default function startHandle() {
+  // monitor message status.
+  monitorMessage();
+
   // receive chunk from REORDER-QUEUE.
   setInterval(() => {
     sqs.receiveMessage({ QueueUrl: process.env.HAI_REORDER_URL }, async (error, data) => {
@@ -123,5 +190,5 @@ export default function startHandle() {
   // send order chunk to HAI-QUEUE
   setInterval(() => {
     reorderChunk();
-  }, 10000);
+  }, 5000);
 }
